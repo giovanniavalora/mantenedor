e91b50c1dd83a72b09c0ef9b7e346ef36195fd4a
giova <giovanni.aravena@avalora.com>
2020-05-04 07:58:47 -0400

    Test edit, create origen-suborigen

--- pages/origenes_suborigenes.vue
+++ pages/origenes_suborigenes.vue
@@ -415,12 +415,13 @@ export default {
     save () {
       if (this.editedIndex > -1) {
         this.editedItem.proyecto = this.idProyecto
-
-        
+        console.log("edit1:",this.origenes[this.editedIndex])
         this.$axios.put(`/Origen/${this.editedItem.id}/`,this.editedItem)
         .then(res => {
           if(res.data){
+            console.log("edit2:",this.origenes[this.editedIndex])
             Object.assign(this.origenes[this.editedIndex], this.editedItem)
+            console.log("edit3:",this.origenes[this.editedIndex])
             this.snack = true
             this.snackColor = 'success'
             this.snackText = 'Actualizado'
@@ -433,12 +434,15 @@ export default {
         });
       } else {
         this.editedItem.proyecto = this.idProyecto
+        console.log("create1:",this.origenes)
         this.$axios.post('/Origen/',this.editedItem)
         .then(res => {
           console.log("new res.data:",res.data)
           if(res.data){
             this.editedItem['id']=res.data['id']
+            console.log("create2:",this.origenes)
             this.origenes.push(this.editedItem)
+            console.log("create3:",this.origenes)
             this.snack = true
             this.snackColor = 'success'
             this.snackText = 'Creado'
