e38a00dd6cb9cfa278a2d5866e451c158795e1e5
giova <giovanni.aravena@avalora.com>
2020-05-22 01:16:50 -0400

    eliminado asyncData flota

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,8 @@ export default {
   */
   axios: {
     // proxy: true,
-    baseURL: "http://127.0.0.1:5000/api/v1"
+    // baseURL: "http://127.0.0.1:5000/api/v1"
+    baseURL: "http://157.245.237.33:5000/api/v1"
     // baseURL: "http://192.168.0.11:5000/api/v1" 
   },
   /*
