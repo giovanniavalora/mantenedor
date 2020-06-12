9fbb90c02986c2e1a85d3aa010dd056ce99a5877
giova <giovanni.aravena@avalora.com>
2020-06-11 21:18:04 -0400

    ssl renew sin dry run

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,5 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/ohl/controlCamionesApi/
-$COMPOSE -f "docker-compose.prod.yml" run certbot renew --dry-run && $COMPOSE -f "docker-compose.prod.yml" kill -s SIGHUP webserver
+$COMPOSE -f "docker-compose.prod.yml" run certbot renew && $COMPOSE -f "docker-compose.prod.yml" kill -s SIGHUP webserver
 $DOCKER system prune -af
